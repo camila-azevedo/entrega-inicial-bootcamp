df72c7515a0ceab39a9f6fd3f1964fa128f99ccf
camila-azevedo <camila.azevedo@sempreceub.com.br>
2026-04-05 12:33:19 -0300

    init commit

---
+++ src/main/java/com/example/githubentregainicial/GithubEntregaInicialApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubentregainicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubEntregaInicialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubEntregaInicialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubentregainicial/GithubEntregaInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubentregainicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubEntregaInicialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
